2391584eafd53704ce8719688f7da98f423479c9
guohaolian <1040071899@qq.com>
2025-12-01 20:17:18 +0800

    feat: 01

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
     <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
     <el-button type="success" size="default" :icon="Edit">编辑按钮</el-button>
     <el-button type="danger" size="default" :icon="Delete">禁用按钮</el-button>
-    <el-pagination v-model:current-page="currentPage4" v-model:page-size="pageSize4" :page-sizes="[100, 200, 300, 400]"
-      :size="size" :disabled="disabled" :background="background" layout="total, sizes, prev, pager, next, jumper"
-      :total="400" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :size="size"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
