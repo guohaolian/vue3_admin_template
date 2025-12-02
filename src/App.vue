9497ada000c34f762d99da6a101120f590ad9c0c
guohaolian <1040071899@qq.com>
2025-12-02 22:20:56 +0800

    feat: 02

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,13 @@ onMounted(() => {
     console.log(res);
   })
 }); */
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 import { reqLogin } from './api/user'
 onMounted(() => {
-  reqLogin({ username: 'admin', password: '111111' }).then(res => {
-    console.log(res);
-  });
-});
+  reqLogin({ username: 'admin', password: '111111' }).then((res) => {
+    console.log(res)
+  })
+})
 </script>
 
 <style scoped lang="scss">
