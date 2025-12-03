019d8dcf38b6cc01aaf7ff4bcd98eae6ec335468
guohaolian <1040071899@qq.com>
2025-12-03 17:37:48 +0800

    feat: 03

--- src/layout/logo/index.vue
+++ src/layout/logo/index.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="logo" v-if="setting.logoHidden">
-        <img :src="setting.logo" alt="">
-        <p>{{ setting.title }}</p>
-    </div>
+  <div class="logo" v-if="setting.logoHidden">
+    <img :src="setting.logo" alt="" />
+    <p>{{ setting.title }}</p>
+  </div>
 </template>
 
 <script setup lang="ts" name="Logo">
 //引入设置标题与logo这配置文件
-import setting from '@/setting';
+import setting from '@/setting'
 </script>
 
 <style scoped lang="scss">
 .logo {
-    width: 100%;
-    height: $base-menu-logo-height;
-    color: rgb(115, 148, 236);
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    opacity: 0.7;
+  width: 100%;
+  height: $base-menu-logo-height;
+  color: rgb(115, 148, 236);
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  opacity: 0.7;
 
-    img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-    }
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
 
-    p {
-        font-size: $base-logo-title-fontSize;
-        margin-left: 10px;
-    }
+  p {
+    font-size: $base-logo-title-fontSize;
+    margin-left: 10px;
+  }
 }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,51 @@
 <template>
-    <div class="login_container">
-        <el-row>
-            <el-col :span="12" :xs="0"></el-col>
-            <el-col :span="12" :xs="24">
-                <!-- 登录的表单 -->
-                <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
-                    <h1>Hello</h1>
-                    <h2>欢迎来到光伏发电功率预测系统</h2>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <!-- 登录的表单 -->
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
+          <h1>Hello</h1>
+          <h2>欢迎来到光伏发电功率预测系统</h2>
 
-                    <el-form-item prop="username">
-                        <el-input :prefix-icon="User" v-model="loginForm.username" placeholder="用户名"></el-input>
-                    </el-form-item>
+          <el-form-item prop="username">
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+              placeholder="用户名"
+            ></el-input>
+          </el-form-item>
 
-                    <el-form-item prop="password">
-                        <el-input :prefix-icon="Lock" v-model="loginForm.password" type="password" show-password
-                            placeholder="密码"></el-input>
-                    </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              type="password"
+              show-password
+              placeholder="密码"
+            ></el-input>
+          </el-form-item>
 
-                    <el-form-item>
-                        <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">
-                            登录
-                        </el-button>
-                    </el-form-item>
-                </el-form>
-            </el-col>
-        </el-row>
-    </div>
+          <el-form-item>
+            <el-button
+              :loading="loading"
+              class="login_btn"
+              type="primary"
+              size="default"
+              @click="login"
+            >
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -44,106 +64,106 @@ let $router = useRouter()
 let useStore = useUserStore()
 let loginForm = reactive({ username: 'admin', password: '111111' })
 //获取el-form组件
-let loginForms = ref();
+let loginForms = ref()
 //定义变量控制按钮加载效果
 let loading = ref(false)
 
 const login = async () => {
-    //保证全部表单相校验通过再发请求
-    await loginForms.value.validate();
-    //加载效果:开始加载
-    loading.value = true
-    try {
-        await useStore.userLogin(loginForm)
-        //编程式导航跳转到展示数据首页
-        $router.push('/')
-        //登录成功提示信息
-        ElNotification({
-            type: 'success',
-            message: '欢迎回来',
-            title: `Hi,${getTime()}好`
-        })
-        //登录成功加载效果也消失
-        loading.value = false
-    } catch (error) {
-        //登录失败加载效果消息
-        loading.value = false
-        //登录失败的提示信息
-        ElNotification({
-            type: 'error',
-            message: (error as Error).message,
-        })
-    }
+  //保证全部表单相校验通过再发请求
+  await loginForms.value.validate()
+  //加载效果:开始加载
+  loading.value = true
+  try {
+    await useStore.userLogin(loginForm)
+    //编程式导航跳转到展示数据首页
+    $router.push('/')
+    //登录成功提示信息
+    ElNotification({
+      type: 'success',
+      message: '欢迎回来',
+      title: `Hi,${getTime()}好`,
+    })
+    //登录成功加载效果也消失
+    loading.value = false
+  } catch (error) {
+    //登录失败加载效果消息
+    loading.value = false
+    //登录失败的提示信息
+    ElNotification({
+      type: 'error',
+      message: (error as Error).message,
+    })
+  }
 }
 
 //自定义校验规则函数
 const validatorUserName = (_: any, value: any, callback: any) => {
-    //rule:即为校验规则对象
-    //value:即为表单元素文本内容
-    //函数:如果符合条件callBack放行通过即为
-    //如果不符合条件callBack方法,注入错误提示信息
-    if (value.length >= 5) {
-        callback();
-    } else {
-        callback(new Error('账号长度至少五位'));
-    }
+  //rule:即为校验规则对象
+  //value:即为表单元素文本内容
+  //函数:如果符合条件callBack放行通过即为
+  //如果不符合条件callBack方法,注入错误提示信息
+  if (value.length >= 5) {
+    callback()
+  } else {
+    callback(new Error('账号长度至少五位'))
+  }
 }
 
 const validatorPassword = (_: any, value: any, callback: any) => {
-    if (value.length >= 6) {
-        callback();
-    } else {
-        callback(new Error('密码长度至少六位'));
-    }
+  if (value.length >= 6) {
+    callback()
+  } else {
+    callback(new Error('密码长度至少六位'))
+  }
 }
 
 //定义表单校验需要配置对象
 const rules = {
-    //规则对象属性:
-    //required,代表这个字段务必要校验的
-    //min:文本长度至少多少位
-    //max:文本长度最多多少位
-    //message:错误的提示信息
-    //trigger:触发校验表单的时机 change->文本发生变化触发校验,blur:失去焦点的时候触发校验规则
-    username: [
-        // { required: true, min: 6, max: 10, message: '账号长度至少六位', trigger: 'change' }
-        { trigger: 'change', validator: validatorUserName }
-    ],
-    password: [
-        // { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' }
-        { trigger: 'change', validator: validatorPassword }
-    ]
+  //规则对象属性:
+  //required,代表这个字段务必要校验的
+  //min:文本长度至少多少位
+  //max:文本长度最多多少位
+  //message:错误的提示信息
+  //trigger:触发校验表单的时机 change->文本发生变化触发校验,blur:失去焦点的时候触发校验规则
+  username: [
+    // { required: true, min: 6, max: 10, message: '账号长度至少六位', trigger: 'change' }
+    { trigger: 'change', validator: validatorUserName },
+  ],
+  password: [
+    // { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' }
+    { trigger: 'change', validator: validatorPassword },
+  ],
 }
 </script>
 <style scoped lang="scss">
 .login_container {
-    width: 100%;
-    height: 100vh;
-    background: url('@/assets/images/background.jpg') no-repeat;
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
     background-size: cover;
+    padding: 40px;
 
-    .login_form {
-        position: relative;
-        width: 80%;
-        top: 30vh;
-        background: url('@/assets/images/login_form.png') no-repeat;
-        background-size: cover;
-        padding: 40px;
-
-        h1 {
-            color: white;
-            font-size: 40px;
-        }
-
-        h2 {
-            font-size: 20px;
-            color: white;
-            margin: 20px 0px;
-        }
-
-        .login_btn {
-            width: 100%;
-        }
+    h1 {
+      color: white;
+      font-size: 40px;
     }
+
+    h2 {
+      font-size: 20px;
+      color: white;
+      margin: 20px 0px;
+    }
+
+    .login_btn {
+      width: 100%;
+    }
+  }
 }
 </style>
