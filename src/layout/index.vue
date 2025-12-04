a6335e76a648082b290c8f879ad4ce6997a5bc70
guohaolian <1040071899@qq.com>
2025-12-04 08:28:29 +0800

    feat: 04

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,31 +1,32 @@
 <template>
-    <div class="layout_container">
-        <!-- 左侧菜单 -->
-        <div class="layout_slider">
-            <logo></logo>
-            <!-- 展示菜单 -->
-            <!-- 滚动组件 -->
-            <el-scrollbar class="scrollbar">
-                <!-- 菜单组件-->
-                <el-menu background-color="#001529" text-color="white" active-text-color="yellowgreen">
-                    <!--根据路由动态生成菜单-->
-                    <Menu :menuList="userStore.menuRoutes"></Menu>
-                </el-menu>
-            </el-scrollbar>
-        </div>
-        <!-- 顶部导航 -->
-        <div class="layout_tabbar">
-            <!-- layout组件的顶部导航tabbar -->
-            bbbbbbbb
-        </div>
-        <!-- 内容展示区域 -->
-        <div class="layout_main">
-
-
-            <Main></Main>
-
-        </div>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider">
+      <logo></logo>
+      <!-- 展示菜单 -->
+      <!-- 滚动组件 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件-->
+        <el-menu
+          background-color="#001529"
+          text-color="white"
+          active-text-color="yellowgreen"
+        >
+          <!--根据路由动态生成菜单-->
+          <Menu :menuList="userStore.menuRoutes"></Menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar">
+      <!-- layout组件的顶部导航tabbar -->
+      bbbbbbbb
+    </div>
+    <!-- 内容展示区域 -->
+    <div class="layout_main">
+      <Main></Main>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -36,62 +37,62 @@ import Menu from '@/layout/menu/index.vue'
 import Main from '@/layout/main/index.vue'
 
 import useUserStore from '@/store/modules/user'
-let userStore = useUserStore();
+let userStore = useUserStore()
 //获取路由对象
 //let $route = useRoute();
 </script>
 
 <style scoped lang="scss">
 .layout_container {
-    width: 100%;
+  width: 100%;
+  height: 100vh;
+
+  .layout_slider {
+    color: white;
+    width: $base-menu-width;
     height: 100vh;
+    background: $base-menu-background;
+    transition: all 0.3s;
 
-    .layout_slider {
-        color: white;
-        width: $base-menu-width;
-        height: 100vh;
-        background: $base-menu-background;
-        transition: all 0.3s;
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
 
-        .scrollbar {
-            width: 100%;
-            height: calc(100vh - $base-menu-logo-height);
-
-            .el-menu {
-                border-right: none;
-            }
-        }
+      .el-menu {
+        border-right: none;
+      }
     }
+  }
 
-    .layout_tabbar {
-        position: fixed;
-        width: calc(100% - $base-menu-width);
-        height: $base-tabbar-height;
-        top: 0px;
-        left: $base-menu-width;
-        transition: all 0.3s;
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+    top: 0px;
+    left: $base-menu-width;
+    transition: all 0.3s;
 
-        &.fold {
-            width: calc(100vw - $base-menu-min-width);
-            left: $base-menu-min-width;
-        }
+    &.fold {
+      width: calc(100vw - $base-menu-min-width);
+      left: $base-menu-min-width;
     }
+  }
 
-    .layout_main {
-        position: absolute;
-        width: calc(100% - $base-menu-width);
-        height: calc(100vh - $base-tabbar-height);
-        left: $base-menu-width;
-        top: $base-tabbar-height;
-        padding: 20px;
-        overflow: auto;
-        transition: all 0.3s;
-        background-color: yellow;
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    padding: 20px;
+    overflow: auto;
+    transition: all 0.3s;
+    background-color: yellow;
 
-        &.fold {
-            width: calc(100vw - $base-menu-min-width);
-            left: $base-menu-min-width;
-        }
+    &.fold {
+      width: calc(100vw - $base-menu-min-width);
+      left: $base-menu-min-width;
     }
+  }
 }
 </style>
