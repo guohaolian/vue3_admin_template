d73e59c226320474918923d2c6a32e8e6d7f0f52
guohaolian <1040071899@qq.com>
2025-12-04 12:23:22 +0800

    feat: 05

--- src/layout/tabbar/index.vue
+++ src/layout/tabbar/index.vue
@@ -1,39 +1,41 @@
 <template>
-    <div class="tabbar">
-        <div class="tabbar_left">
-            <Breadcrumb></Breadcrumb>
-
-        </div>
-        <div class="tabbar_right">
-            <Setting></Setting>
-        </div>
+  <div class="tabbar">
+    <div class="tabbar_left">
+      <Breadcrumb></Breadcrumb>
     </div>
-
+    <div class="tabbar_right">
+      <Setting></Setting>
+    </div>
+  </div>
 </template>
 
-<script setup lang='ts' name='Tabbar'>
+<script setup lang="ts" name="Tabbar">
 import Breadcrumb from './breadcrumb/index.vue'
 import Setting from './setting/index.vue'
 </script>
 
 <style scoped lang="scss">
 .tabbar {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-image: linear-gradient(
+    to right,
+    rgb(208, 227, 240),
+    rgb(104, 153, 243),
+    rgb(63, 110, 240)
+  );
+
+  .tabbar_left {
     display: flex;
-    justify-content: space-between;
-    background-image: linear-gradient(to right, rgb(208, 227, 240), rgb(104, 153, 243), rgb(63, 110, 240));
+    align-items: center;
+    margin-left: 20px;
+  }
 
-    .tabbar_left {
-        display: flex;
-        align-items: center;
-        margin-left: 20px;
-
-    }
-
-    .tabbar_right {
-        display: flex;
-        align-items: center;
-    }
+  .tabbar_right {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,26 +1,26 @@
 <template>
-    <el-button size="small" icon="Refresh" circle></el-button>
-    <el-button size="small" icon="FullScreen" circle></el-button>
-    <el-button size="small" icon="Setting" circle></el-button>
-    <img src="../../../../public/logo.png" style="width: 30px;height: 30px;border-radius: 50%;margin-left: 10px;">
-    <el-dropdown style="margin-left: 10px;">
-        <span class="el-dropdown-link">
-            admin
-            <el-icon class="el-icon--right">
-                <arrow-down />
-            </el-icon>
-        </span>
-        <template #dropdown>
-            <el-dropdown-menu>
-                <el-dropdown-item>退出登录</el-dropdown-item>
-
-            </el-dropdown-menu>
-        </template>
-    </el-dropdown>
+  <el-button size="small" icon="Refresh" circle></el-button>
+  <el-button size="small" icon="FullScreen" circle></el-button>
+  <el-button size="small" icon="Setting" circle></el-button>
+  <img
+    src="../../../../public/logo.png"
+    style="width: 30px; height: 30px; border-radius: 50%; margin-left: 10px"
+  />
+  <el-dropdown style="margin-left: 10px">
+    <span class="el-dropdown-link">
+      admin
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
 
-<script setup lang='ts' name='Setting'>
-
-</script>
+<script setup lang="ts" name="Setting"></script>
 
 <style></style>
